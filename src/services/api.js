76ff7b47d430efaef7b637a11e986d715ade76b8
CASilva <casilva.com@gmail.com>
2021-04-03 22:53:52 -0300

    change port ps

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 import { getToken } from "./auth";
 
 const token = getToken();
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    headers: {"Authorization" : `Bearer ${token}`}
+    baseURL: "http://localhost:5432",
+    headers: { Authorization: `Bearer ${token}` },
 });
 
 export default api;
