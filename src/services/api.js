be5b7c8040e7bd123a6ac5345ab7507e31b52b0b
CASilva <casilva.com@gmail.com>
2021-04-03 22:55:39 -0300

    change port ps

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import { getToken } from "./auth";
 const token = getToken();
 
 const api = axios.create({
-    baseURL: "http://localhost:5432",
+    baseURL: "ec2-54-235-108-217.compute-1.amazonaws.com:5432",
     headers: { Authorization: `Bearer ${token}` },
 });
 
